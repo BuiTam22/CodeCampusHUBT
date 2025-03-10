2555143c923fd98eea70ec7ff41d8f2a3b9a66ee
Bùi Văn Tâm <tamse22032004@gmail.com>
2025-03-10 23:05:32 +0700

    Add CodingAchievement,....

--- NCKH2024TQQD/src/main/java/com/codecampushubt/NCKH2024TQQD/entity/UserRanking.java
+++ NCKH2024TQQD/src/main/java/com/codecampushubt/NCKH2024TQQD/entity/UserRanking.java
@@ -1,8 +1,14 @@
 package com.codecampushubt.NCKH2024TQQD.entity;
 
-import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "UserRankings")
