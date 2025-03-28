5e5ff205485140e5fed362aa6c774089b4f67b83
Your duy <you@example.com>
2025-03-28 23:37:16 +0700

    a

---
+++ NCKH2024TQQD/src/main/java/com/codecampushubt/NCKH2024TQQD/Controller/Admin/Dashboard/show.java
@@ -0,0 +1,15 @@
+package com.codecampushubt.NCKH2024TQQD.Controller.Admin.Dashboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin/dashboard")
+public class show {
+    @GetMapping("/show")
+    public String showDashboard(){
+        return "dashboard/show";
+
+    }
+}
