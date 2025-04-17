dc8f51fc188a598b549c3fdb591ae4a46bd4c4a8
Bùi Văn Tâm <tamse22032004@gmail.com>
2025-04-17 14:48:42 +0700

    tạo api để lấy ra chi tiết bài tập

--- NCKH2024TQQD/src/main/java/com/codecampushubt/NCKH2024TQQD/service/CodingExerciseServices/CodingExerciseService.java
+++ NCKH2024TQQD/src/main/java/com/codecampushubt/NCKH2024TQQD/service/CodingExerciseServices/CodingExerciseService.java
@@ -1,9 +1,14 @@
 package com.codecampushubt.NCKH2024TQQD.service.CodingExerciseServices;
 
 import com.codecampushubt.NCKH2024TQQD.dto.CodingExerciseDTO.CodingExerciseDTO;
+import com.codecampushubt.NCKH2024TQQD.dto.CodingExerciseDTO.CodingExerciseDetailDTO;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CodingExerciseService {
     List<CodingExerciseDTO> getCodingExerciseDTOByLessonSlug(String theSlug);
+
+    // Lấy ra chi tiết bài tập dựa vào slug của bài tập
+    CodingExerciseDetailDTO getCodingExerciseDetailDTOByExerciseSlug(String theSlug);
 }
