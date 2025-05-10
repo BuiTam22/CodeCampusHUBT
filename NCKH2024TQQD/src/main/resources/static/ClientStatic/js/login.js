14ca08572b70a6009163c68f570336c97ef7e08d
Your duy <you@example.com>
2025-05-10 20:52:02 +0700

    fe gửi data

--- NCKH2024TQQD/src/main/resources/static/ClientStatic/js/login.js
+++ NCKH2024TQQD/src/main/resources/static/ClientStatic/js/login.js
@@ -39,3 +39,46 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
         submitBtn.querySelector(".button-text").textContent = originalText;
     }
 });
+
+//ẩn Hiện Form
+
+function hideAllForm() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registerForm").style.display = "none";
+    document.getElementById("forgotForm").style.display = "none";
+    document.getElementById("navButtons").style.display = "none";
+}
+
+function showFormLogin() {
+    hideAllForm();
+    document.getElementById("loginForm").style.display = "block";
+    document.getElementById("navButtons").style.display = "block";
+}
+
+function showFormRegister() {
+    hideAllForm();
+    document.getElementById("registerForm").style.display = "block";
+}
+
+function showFormForgot() {
+    hideAllForm();
+    document.getElementById("forgotForm").style.display = "block";
+}
+
+//end Ẩn Hiện Form
+
+//regiter
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    // console.log(formData)
+    const data = {
+        email:formData.get("email"),
+        fullName:formData.get("fullName"),
+        userName:formData.get("username"),
+        password: formData.get("password")
+
+    }
+    console.log(data)
+})
+//end regitter
